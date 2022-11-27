1e7c5cc74e5abdb34384fab5417acdf4798babb0
PovilasGe <povilasgedminas@gmail.com>
2022-11-27 13:20:53 +0200

    add countries page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import CountriesPage from './pages/CountriesPage';
+import HomePage from './pages/HomePage';
+import Navigation from './components/Navigation/Navigation';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='page-wrapper'>
+      <Navigation />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/countries' element={<CountriesPage />} />
+      </Routes>
     </div>
   );
 }
